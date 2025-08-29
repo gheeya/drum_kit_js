dd2b1862db626c48af56ab523175ca87713e2122
Rai <anshav.rai@capgemini.com>
2025-08-29 08:14:34 +0530

    Refactored the code and added new logic

--- script.js
+++ script.js
@@ -2,29 +2,43 @@ const audios = document.querySelectorAll(".drum-audio");
 const drumContainer = document.querySelector(".drum-container");
 const drums = document.querySelectorAll(".drum");
 
-window.addEventListener("keydown", function (evt) {
-  const keyInRange = [...drums].some(
-    (drum) => drum.dataset.key === evt.key.toUpperCase()
-  );
-  if (keyInRange) {
-    const ele = [...drums].find(
-      (drum) => drum.dataset.key === evt.key.toUpperCase()
-    );
-    const audioClass = ele
-      .querySelector(".key-sound")
-      .textContent.toLowerCase();
-    const audio = document.querySelector(`.${audioClass}`);
-    console.log(audio);
-    audio.play();
-    ele.classList.add("playing");
-  }
-});
-window.addEventListener("keyup", function (evt) {
-  const ele = [...drums].find(
-    (drum) => drum.dataset.key === evt.key.toUpperCase()
-  );
-  const audioClass = ele.querySelector(".key-sound").textContent.toLowerCase();
-  const audio = document.querySelector(`.${audioClass}`);
-  audio.pause();
-  ele.classList.remove("playing");
-});
+function playSound(evt) {
+  // const keyInRange = [...drums].some(
+  //   (drum) => drum.dataset.key === evt.key.toUpperCase()
+  // );
+  // if (keyInRange) {
+  //   const ele = [...drums].find(
+  //     (drum) => drum.dataset.key === evt.key.toUpperCase()
+  //   );
+  //   const audioClass = ele
+  //     .querySelector(".key-sound")
+  //     .textContent.toLowerCase();
+  //   const audio = document.querySelector(`.${audioClass}`);
+  //   // console.log(audio);
+  //   audio.currentTime = 0;
+  //   audio.play();
+  //   ele.classList.add("playing");
+  // }
+  const audio = document.querySelector(`audio[data-key="${evt.keyCode}"]`);
+  const ele = document.querySelector(`div[data-key="${evt.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  ele.classList.add("playing");
+}
+
+function pauseSound(evt) {
+  // const ele = [...drums].find(
+  //   (drum) => drum.dataset.key === evt.key.toUpperCase()
+  // );
+  // const audioClass = ele.querySelector(".key-sound").textContent.toLowerCase();
+  // const audio = document.querySelector(`.${audioClass}`);
+  // ele?.classList.remove("playing");
+  if (!(evt.propertyName === "transform" || evt.propertyName === "box-shadow"))
+    return;
+  this.classList.remove("playing");
+}
+
+window.addEventListener("keydown", playSound);
+
+drums.forEach((drum) => drum.addEventListener("transitionend", pauseSound));
